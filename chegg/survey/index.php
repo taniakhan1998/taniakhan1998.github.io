afaa0f1bb8e3a932f832f5449f5e692bc67fe466
taniakhan1998 <taniakhan61998@gmail.com>
2024-12-05 19:18:59 +1345

    Delete chegg directory

--- chegg/survey/index.php
+++
@@ -1,7 +0,0 @@
-
-
-    <title>Please Complete a Survey Below to Unlock.</title>
-
-<script type="text/javascript" src="https://utopicmobile.com/script_include.php?id=1347204"></script>								
-						
-					
